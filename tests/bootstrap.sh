35d460e94dc4441a9bd4903405de58cd5ffc0355
David Gowers <00ai99@gmail.com>
2009-08-04 17:15:50 +0930

    tests: bootstrap: linearize .l too

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -5,5 +5,6 @@
 
 ./linearize data/viking.dox 0 200 3 data/viking.dox.linear
 ./linearize data/viking.d01 7 400 5 data/viking.d01.linear
+./linearize data/viking.l01 7 300 5 data/viking.l01.linear
 ./linearize data/viking.til 0 16000 4 data/viking.til.linear
 ./spliceattack data/viking.dt6 data/attack.bin data/attack.full
