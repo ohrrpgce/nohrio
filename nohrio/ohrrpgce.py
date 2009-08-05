bda00a5c4f1a6ea24fa9ef75a0ce2341710550a2
David Gowers <00ai99@gmail.com>
2009-08-05 15:47:16 +0930

    ohrrpgce: convert npcdef dtype from a fixed, shape = (100,) dtype to a scalar dtype.

--- nohrio/ohrrpgce.py
+++ nohrio/ohrrpgce.py
@@ -572,10 +572,10 @@ dtypes = {
                         name = fvstr (20), bitsets = ('B', 2),
                         offset = xycoord_dtype, anchor = xycoord_dtype),
     'mn' : [('name', [('length', 'B'), ('value', 'S79')])],
-    'n' : [BLOAD_HEADER, ('npcs', (make ('picture palette movetype speed showtext',
-                                         'activate_action giveitem pushability activation',
-                                         'appear_if_tag1 appear_if_tag2 usability trigger',
-                                         'script_arg vehicle'), 100))],
+    'n' : [('npc', make ('picture palette movetype speed showtext',
+                         'activate_action giveitem pushability activation',
+                         'appear_if_tag1 appear_if_tag2 usability trigger',
+                         'script_arg vehicle'))],
 
 # .. _pal:
 #
