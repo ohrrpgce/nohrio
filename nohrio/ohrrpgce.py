6162ebcc70883131d40bd0f98bc3ea982c7a181f
David Gowers <00ai99@gmail.com>
2009-08-07 21:25:55 +0930

    ohrrpgce, ohrstring: refactor string mangling into a separate submodule

--- nohrio/ohrrpgce.py
+++ nohrio/ohrrpgce.py
@@ -76,6 +76,7 @@
 
 import numpy as np
 import os
+from ohrstring import *
 
 # Helper data
 # ----------------
@@ -326,35 +327,6 @@ class archiNym (object):
     prefix = property (_getprefix, _setprefix)
     version = property (_getversion, _setversion)
 
-
-# String de/mangling
-# ===================
-#
-# Functions for setting and getting 8 and 16-bit strings
-# from numpy array-ish objects.
-#
-# Unlike OHRRPGCE's editor, these are 'clean' -- set operations always
-# wipe unused bytes.
-#
-# If you want to set multiple strings, create an array of the appropriate
-# dtype -- ``[('length', int16 or int8), ('value', int16 or int8, maxlength)]``
-# , store your values with set_*, and assign them to the target array range.
-
-def set_str16 (dest, src): # 16bit/8bit len, 16bit chars
-    assert len (src) <= (len (dest['value']) / 2)
-    dest['length'] = len(src)
-    dest['data'] = "".join([char + '.' for char in src] + ['\x00\x00'])
-
-def set_str8 (dest, src): # 16 or 8bit header, 8bit chars
-    assert len (src) <= len (dest['value'])
-    dest['length'] = len(src)
-    dest['value'] = src + '\x00'
-
-def get_str16 (src):
-    return src['data'][::2]
-
-def get_str8 (src):
-    return src['value']
 
 def seq_to_str (src):
     """convert a length,..,[charvalue,charvalue,..] sequence to a normal string."""
