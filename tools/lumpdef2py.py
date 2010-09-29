2ff4891478b86f79301256a9b4433206f756e7d6
David Gowers <00ai99@gmail.com>
2010-09-29 12:53:04 +0930

    Start of lumpdef compiler

---
+++ tools/lumpdef2py.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import yaml
+
+META = {}
+
+class Compiler (object):
+    def __init__ (self, filename):
+        self.source_filename = filename
+        f = open (filename, 'rb')
+        self.source = yaml.safe_load (f)
+        f.close ()
+        self.classify ()
+    def classify (self):
+        d = self.source
+        self.helperdata = {}
+        [d[k] for k in d.keys() if k == k.upper()]
+        self.dtypeinfo = {}
+        for k, v in d.items():
+            if k == k.upper():
+                self.helperdata[k] = v
+            elif k == k.lower():
+                self.dtypeinfo[k] = v
+            else:
+                print ('unknown data kind %r = %r' % (k, v))
+
+from pprint import pprint
+
+c = Compiler ('ohr-lumpdefs.yaml')
+pprint (c.helperdata)
+
