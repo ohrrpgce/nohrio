d1e3e69ee1785f4645cc3d5325a5f547565c81a4
David Gowers <00ai99@gmail.com>
2012-10-21 10:34:25 +1030

    test_reload: fill out 8 or so more reading unittests

--- tests/dtypes/test_reload.py
+++ tests/dtypes/test_reload.py
@@ -10,9 +10,11 @@ from nohrio.dtypes.reload import RELOAD, ReadNode, WriteNode
 from testutils import loadsaveok
 
 class testReadReload(TestCase):
+    def setup(self):
+        self.rld = RELOAD(self.simple_testdata, 'r')
     def testOpen(self):
         """Open simple document"""
-        tmp = RELOAD(self.simple_testdata, 'r')
+
         root = tmp['root']
         ok([c.name for c in root.children]) == ['simple', 'nested']
         ok(root['simple']) == 42
@@ -23,13 +25,14 @@ class testReadReload(TestCase):
 
     def testSingleAccess(self):
         """[existing_simple_node_name] -> simple type"""
-        tmp = RELOAD(self.simple_testdata, 'r')
+        tmp = self.rld
         root = tmp['root']
         ok(type(root['simple'])) == 42
 
     def testNoexistAccess(self):
         """[nonexisting_name] raises KeyError """
-        assert 0
+        tmp = self.rld
+        ok(lambda:tmp['fruit']).raises(KeyError)
 
     def testMultiaccessLen(self):
         """[multiple, existing, keys] -> seq of ReadNode, len() == nkeys"""
@@ -37,31 +40,42 @@ class testReadReload(TestCase):
 
     def testMultinoexistAccess(self):
         """[multiple, keys, some, not, present] raises KeyError"""
-        assert 0
+
+        ok(lambda: tmp['root']['simple', 'nested', 'kamina']).raises(KeyError)
 
     def testSub(self):
         """.sub(name) -> ReadNode(name = name)"""
-        assert 0
+        tmp = self.rld
+        ok(tmp.sub('root').__class__) == ReadNode
 
     def testSubExistVal(self):
         """.sub(name, existing_value) -> ReadNode(value = existing_value)"""
-        assert 0
+        tmp = self.rld
+        root = tmp['root']
+        ok(root.sub('simple', 42).value) == 42
+
 
     def testSubNoexistVal(self):
         """.sub(name, nonexisting_value) raises ValueError """
-        assert 0
+        tmp = self.rld
+        root = tmp['root']
+        ok(lambda:root.sub('simple', 24)).raises(ValueError)
 
     def testNoexistSub(self):
         """.sub(nonexisting_name) raises KeyError"""
-        assert 0
+        tmp = self.rld
+        root = tmp['root']
+        ok(lambda:root.sub('fruit')).raises(KeyError)
 
     def testStr(self):
         """.str() -> str if type(self) == bytes"""
-        assert 0
+        tmp = self.rld
+        ok(type(tmp['root'].sub('bites').str())) == str
 
     def testNonStr(self):
         """.str() raises TypeError if type(self) != bytes"""
-        assert 0
+        tmp = self.rld
+        ok(lambda:type(tmp['root'].sub('simple').str())).raises(TypeError)
 
     def testIter(self):
         """.iter() returns a sequence of ReadNodes w/ appropriate names"""
@@ -77,19 +91,39 @@ class testReadReload(TestCase):
 
     def testPathKey(self):
         """[path] looks up nested nodes correctly"""
-        assert 0
+        tmp = self.rld
+        ok(tmp['root/nested/count']) == tmp['root']['nested']['count']
+
+    def testPathRelError(self):
+        """[relative_path] raises KeyError"""
+        tmp = self.rld
+        ok(lambda:tmp['root']['nested']['../nested/count']).raises(KeyError)
 
     def testPath(self):
         """.path returns a usable node path"""
+        tmp = self.rld
+        ok(tmp.sub('root').path) == 'root'
+        ok(tmp['root'].sub('nested').path) == 'root/nested'
+        ok(tmp['root']['nested'].sub('count').path) == 'root/nested/count'
+
+    def testBlankPathItem(self):
+        """.path returns a usable node path when blank-named nodes are in the path"""
         assert 0
 
     def testIndices(self):
         """.indices returns a usable indices tuple"""
-        assert 0
+
+        ok(root.sub('simple').indices) == (0, 0)
+        ok(root['nested'].sub('count').indices) == (0, 1, 0)
 
     def testContains(self):
         """`name in self` works """
-        assert 0
+        tmp = self.rld
+        ok(lambda v:'root' in tmp) == True
+        ok(lambda v:'simple' in tmp) == False
+        ok(lambda v:'simple' in tmp['root']) == True
+        ok(lambda v:'nested' in tmp['root']) == True
+        ok(lambda v:'count' in tmp['root']['nested']) == True
 
     def testCount(self):
         """.count(name) -> correct count """
